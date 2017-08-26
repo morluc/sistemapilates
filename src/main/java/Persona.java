5c7d9553cb29f6575f7fa5f3e10b01fb709d09ca
Lucas Gonzalez <lucasgxnzalez@gmail.com>
2017-08-26 17:01:49 -0300

    TODO de la clase Persona terminado

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -1,4 +1,58 @@
 public abstract class Persona {
     // Atributos de instancia: tipoDni(int), dni(int), nombre(String), apellido(String)
-    // TODO: Generar constructor, getters y setters
+    private int tipoDni, dni;
+    private String nombre, apellido;
+
+    public Persona(int t, int d, String n, String a)
+    {
+      tipoDni = t;
+      dni = d;
+      nombre = n;
+      apellido = a;
+    }
+
+    //comandos
+    public void cambiarTipoDni(int t)
+    {
+      tipoDni = t;
+    }
+
+    public void cambiarDni(int d)
+    {
+      dni = d;
+    }
+
+    public void cambiarNombre(String n)
+    {
+      nombre = n;
+    }
+
+    public void cambiarApellido(String a)
+    {
+      apellido = a;
+    }
+
+    //consultas
+
+    public int obtenerTipoDni()
+    {
+      return tipoDni;
+    }
+
+    public int obtenerDni()
+    {
+      return dni;
+    }
+
+    public String obtenerNombre()
+    {
+      return nombre;
+    }
+
+    public String obtenerApellido()
+    {
+      return apellido;
+    }
+
+
 }
